43c69070e832c4b85798a2a458f2f1e5780414d5
unknown <muhammad_farrel_27rpl@student.smktelkom-mlg.sch.id>
2021-03-02 09:22:35 +0700

    comment

---
+++ tema_crud/home.php
@@ -0,0 +1,5 @@
+<center><br><br><br><br><br><br><br><br>
+<img src="assets/images/faviconn.png" width="200px height="200px" /> <br>
+<font Size="6" face="Helvetica">afless.id</font> <br>
+<font Size="6">Data Karyawan</font>
+</center>
